42d4f6d4a4ae6692249605eb9ec526f9ccdfd10a
root <root@ip-172-31-18-234.us-west-2.compute.internal>
2023-02-19 14:45:55 +0000

    added crossregion

--- lib/stages/amplify-stage.ts
+++ lib/stages/amplify-stage.ts
@@ -1,18 +1,1 @@
-import { CfnOutput, cfnTagToCloudFormation, Construct, Stage, StageProps } from '@aws-cdk/core';
-// IMPORT AMPLIFY EXPORTED BACKEND HERE
-import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
-import * as path from 'path'
-import * as cdk from '@aws-cdk/core'
-
-export class AmplifyStage extends Stage {
-  
-  constructor(scope: Construct, id: string, props?: StageProps) {
-    super(scope, id, props);
-    
-    // ADD AMPLIFY EXPORTED BACKEND STACK HERE
-     const amplifyStack = new AmplifyExportedBackend(this, "amplifyexportedbackend", {
-      path: path.resolve(__dirname, '..', 'amplify-export-mytodoapp'),
-      amplifyEnvironment: "dev"
-    })
-  }
-}
+    region: "us-west-2", // or "us-east-1"
